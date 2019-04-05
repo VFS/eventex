a1ee36a9503843e0a4d6d34d698106a37f452eb1
Victor Fischer Scattone <git@vfs.io>
2019-04-05 20:08:25 -0300

    Tela de inscricao

---
+++ eventex/subscriptions/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+class SubscriptionForm(forms.Form):
+    name = forms.CharField(label="Nome")
+    cpf = forms.CharField(label="CPF")
+    email = forms.EmailField(label="E-Mail")
+    phone = forms.CharField(label="Phone")
