ba13aa210a1e613cfe146fdab2bc3ae85821dc70
Victor Fischer Scattone <git@vfs.io>
2019-04-11 21:57:10 -0300

    Incluir django admin

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -2,8 +2,22 @@ from django.db import models
 
 
 class Subscription(models.Model):
-    name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = "inscrição"
+        verbose_name_plural = "inscrições"
+        ordering = ('-created_at',)
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name="nome")
+    cpf = models.CharField(
+        max_length=11,
+        verbose_name="CPF")
+    email = models.EmailField(verbose_name="e-mail")
+    phone = models.CharField(
+        max_length=20,
+        verbose_name="telefone")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="criado em")
+
+    def __str__(self):
+        return self.name
